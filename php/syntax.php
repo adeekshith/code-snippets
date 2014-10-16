336776dadfd7f3e400e868de23e880078daf3b0f
Deekshith Allamaneni <dkh.hyd@gmail.com>
2014-10-16 10:38:33 -0500

    PHP Basic example

---
+++ php/syntax.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Basic PHP Example</title>>
+</head>>
+<body>
+
+	<h1>My first PHP page</h1>
+	
+	<?php
+		echo "Hello World!";
+	?>
+	<?php
+		$x=5;
+		$y=6;
+		$z=$x+$y;
+		echo $z;
+	?> 
+	</body>
+</html>
